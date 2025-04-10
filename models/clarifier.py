717ab2e3691e0111f0e729d6584504b5a9431521
Chase Mcclellan <500196790@fvtc.edu>
2025-04-10 14:04:09 -0500

    init branch commit + Added script comments and requirements

---
+++ models/clarifier.py
@@ -0,0 +1,8 @@
+'''
+#clarification script
+Then Chase, you can build the clarification script.
+It will read from that ambiguity report, and for any requirement marked as vague,
+it will call the LLM to generate a better version, saving it to output/refined_requirements.json.
+'''
+
+
